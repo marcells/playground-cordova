83b56f0f8e9136d4c5df6650d9168154411e3e50
Marcell Spies <marcells@gmx.de>
2014-07-22 00:16:31 +0200

    Added sliding animation between views.

--- workshop/platforms/wp8/www/js/app.js
+++ workshop/platforms/wp8/www/js/app.js
@@ -6,17 +6,19 @@
     EmployeeListView.prototype.template = Handlebars.compile($("#employee-list-tpl").html());
     EmployeeView.prototype.template = Handlebars.compile($("#employee-tpl").html());
 
+    var slider = new PageSlider($('body'));
+
     var service = new EmployeeService();
     service.initialize().done(function () {
         console.log("Service initialized");
         
         router.addRoute('', function() {
-            $('body').html(new HomeView(service).render().$el);
+            slider.slidePage(new HomeView(service).render().$el);
         });
 
         router.addRoute('employees/:id', function(id) {
             service.findById(parseInt(id)).done(function(employee) {
-                $('body').html(new EmployeeView(employee).render().$el);
+                slider.slidePage(new EmployeeView(employee).render().$el);
             });
         });
 
