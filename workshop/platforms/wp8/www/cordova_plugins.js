256bb7443bdfe534ac9489f1945105c68e6a7cc0
Marcell Spies <marcells@gmx.de>
2014-07-22 00:37:04 +0200

    Added camera support.

--- workshop/platforms/wp8/www/cordova_plugins.js
+++ workshop/platforms/wp8/www/cordova_plugins.js
@@ -87,6 +87,34 @@ module.exports = [
     {
         "file": "plugins/org.apache.cordova.contacts/www/ContactFieldType.js",
         "id": "org.apache.cordova.contacts.ContactFieldType"
+    },
+    {
+        "file": "plugins/org.apache.cordova.camera/www/CameraConstants.js",
+        "id": "org.apache.cordova.camera.Camera",
+        "clobbers": [
+            "Camera"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.camera/www/CameraPopoverOptions.js",
+        "id": "org.apache.cordova.camera.CameraPopoverOptions",
+        "clobbers": [
+            "CameraPopoverOptions"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.camera/www/Camera.js",
+        "id": "org.apache.cordova.camera.camera",
+        "clobbers": [
+            "navigator.camera"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.camera/www/CameraPopoverHandle.js",
+        "id": "org.apache.cordova.camera.CameraPopoverHandle",
+        "clobbers": [
+            "CameraPopoverHandle"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -96,7 +124,8 @@ module.exports.metadata =
     "org.apache.cordova.device": "0.2.10",
     "org.apache.cordova.dialogs": "0.2.8",
     "org.apache.cordova.geolocation": "0.3.8",
-    "org.apache.cordova.contacts": "0.2.11"
+    "org.apache.cordova.contacts": "0.2.11",
+    "org.apache.cordova.camera": "0.3.0"
 }
 // BOTTOM OF METADATA
 });
